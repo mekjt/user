c2d370201891d34e6a2e76ca16f98ef61fd9d662
mekjt <gugjunghwa2@gmail.com>
2026-07-22 11:57:24 +0900

    LL

--- Assets/C#/Tower.cs
+++ Assets/C#/Tower.cs
@@ -4,12 +4,15 @@ public class Tower : MonoBehaviour
 {
     int Ehp;
     Enemy enemytrigger;
-    public Tower tower;
     public Towerall towerall;
     public Towerall.allTowernumbers allTowernumbers;
 
     void OnTriggerEnter(Collider other)
     {
-        enemytrigger = other.GetComponent<Enemy>();
+        if(enemytrigger = other.GetComponent<Enemy>())
+        {
+            Debug.Log("경고!!!!");
+        }
+
     }
 }
