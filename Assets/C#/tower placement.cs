b29e7d90461e01940ca83620603cecd52054dc4d
mekjt <gugjunghwa2@gmail.com>
2026-07-23 17:25:52 +0900

    //

--- Assets/C#/tower placement.cs
+++ Assets/C#/tower placement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
@@ -10,20 +11,24 @@ public class TowerPlacement : MonoBehaviour
     private void Awake()
     {
         mainCamera = Camera.main;
-        mainCamera.ScreenPointToRay(Input.mousePosition);
     }
 
     void Update()
     {
 
         // 마우스 좌클릭이 처음 들어온 순간 (GetMouseButtonDown과 같음)
-        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
-        {
-            Debug.Log("마우스 클릭됨!");
-            
-            Vector2 mousePos = Mouse.current.position.ReadValue();
-            Debug.Log($"마우스 좌표: {mousePos}");
-        }
+        //if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        //{
+        //    Ray black = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        //    Ray black = out Hit
+        //
+        //   Vector2 mousePos = Mouse.current.position.ReadValue();
+        //    Debug.Log($"마우스 좌표: {mousePos}");
+        //}
+        //if (Physics.Raycast)
+        //{
+        //    (Blackboard, out Hit
+        //}
     }
 } 
 
